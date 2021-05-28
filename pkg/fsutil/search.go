2becd703481c19da7e8e0c39d56772c0062155be
kas <exactly-one-kas@users.noreply.github.com>
2021-05-28 16:55:23 +0200

    Implement case insensitive command and file completion for Windows

--- pkg/fsutil/search.go
+++ pkg/fsutil/search.go
@@ -16,29 +16,16 @@ func DontSearch(exe string) bool {
 		strings.ContainsRune(exe, '/')
 }
 
-// IsExecutable determines whether path refers to an executable file.
-func IsExecutable(path string) bool {
-	fi, err := os.Stat(path)
-	if err != nil {
-		return false
-	}
-	fm := fi.Mode()
-	return !fm.IsDir() && (fm&0111 != 0)
-}
-
 // EachExternal calls f for each name that can resolve to an external command.
 //
 // BUG: EachExternal may generate the same command multiple command it it
 // appears in multiple directories in PATH.
-//
-// BUG: EachExternal doesn't work on Windows since it relies on the execution
-// permission bit, which doesn't exist on Windows.
 func EachExternal(f func(string)) {
 	for _, dir := range searchPaths() {
 		// TODO(xiaq): Ignore error.
 		infos, _ := ioutil.ReadDir(dir)
 		for _, info := range infos {
-			if !info.IsDir() && (info.Mode()&0111 != 0) {
+			if IsExecutableByInfo(info) {
 				f(info.Name())
 			}
 		}
@@ -46,5 +33,5 @@ func EachExternal(f func(string)) {
 }
 
 func searchPaths() []string {
-	return strings.Split(os.Getenv(env.PATH), ":")
+	return strings.Split(os.Getenv(env.PATH), string(filepath.ListSeparator))
 }
